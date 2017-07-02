2dea799bd8518cbc6eaa409ac157a43609148108
pranavj1001 <pranavj1001@gmail.com>
2017-07-02 12:17:02 +0530

    get necessary keyboard codes

--- NumberWizard/Assets/Scripts/NumberWizard.cs
+++ NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -15,10 +15,23 @@ public class NumberWizard : MonoBehaviour {
 		print ("The highest number you can pick is " + maximumNumber);
 		print ("The lowest number you can pick is " + minimumNumber);
 		
+		print ("Is the number higher or lower than 500?");
+		print ("UP arrow for higher, DOWN for lower, ENTER for equal");
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+			print("Lower it is then");
+			
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+			print("Higher it is then");
+			
+		if (Input.GetKeyDown(KeyCode.Return))
+			print("Equal it is then");
+		
 	
 	}
 }
