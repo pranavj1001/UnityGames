b257d575c3c9fc36c7700613c85aaed28649cf8a
pranavj1001 <pranavj1001@gmail.com>
2017-07-04 02:29:58 +0530

    add the prison image asset and a textController

---
+++ Text101/Assets/TextController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+// to bring up the UI information eg: Here we need the Text
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+	
+		text.text = "Dovah here!";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown(KeyCode.Space)){
+			text.text = "Pranav here!";
+		}
+		
+	}
+}
