b42880f0f6e281b9a2b22aa029b845551de219c4
pranavj1001 <pranavj1001@gmail.com>
2017-07-27 00:43:20 +0530

    the main logic and game is ready

--- NumberWizard/Assets/Scripts/NumberWizard.cs
+++ NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -1,71 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class NumberWizard : MonoBehaviour {
 
 	int maximumNumber;
 	int minimumNumber;
 	int guess;
-
+	int maxGuessesAllowed = 10;
+	
+	public Text text;
+	
 	// Use this for initialization
 	void Start () {
-		
-		InitializeVariables();
 		StartGame();
+	}
+	
+	// Assign values to the variables
+	void StartGame(){
+		
+		maximumNumber = 10000;
+		minimumNumber = 1;
+		guess = 500;
+		
+		maximumNumber++;
 		
 	}
 	
-	// Initialize the Game
-	void StartGame(){
-		
-		print("========================");
-		print("Welcome to Number Wizard");
-		print("Pick a number in your head, but don't tell me!");
-		
-		guess = maximumNumber/2;
-		
-		print("The highest number you can pick is " + maximumNumber);
-		print("The lowest number you can pick is " + minimumNumber);
-		maximumNumber++;
-		
-		print("Is the number " + guess + "?");
-		print("UP arrow for higher, DOWN for lower, ENTER for equal");
-		
+	public void GuessHigher(){
+		minimumNumber  = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower(){
+		maximumNumber = guess;
+		NextGuess();
 	}
 	
 	// Calculate the next guess
 	void NextGuess(){
 		
 		guess = (minimumNumber + maximumNumber) / 2;
-		print("Is it " + guess + "?");
-		print("UP arrow for higher, DOWN for lower, ENTER for equal");
+		text.text= guess.ToString();
+		maxGuessesAllowed--;
 		
-	}
-	
-	// Assign values to the variables
-	void InitializeVariables(){
-		
-		maximumNumber = 10000;
-		minimumNumber = 1;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-		if (Input.GetKeyDown(KeyCode.DownArrow)){
-			//print("Lower it is then");
-			maximumNumber = guess;
-			NextGuess();
-		}else if (Input.GetKeyDown(KeyCode.UpArrow)){
-			//print("Higher it is then");
-			minimumNumber = guess;
-			NextGuess();
-		}else if (Input.GetKeyDown(KeyCode.Return)){
-			print("The Number is " + guess);
-			InitializeVariables();
-			StartGame();
+		if(maxGuessesAllowed <= 0){
+			Application.LoadLevel("Win");
 		}
-	
+		
+		
 	}
 }
