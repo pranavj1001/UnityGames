2ba1d7dd7aa84865f6ce2483ccd5ddf5c1d3cf26
pranavj1001 <pranavj1001@gmail.com>
2017-08-24 22:16:21 +0530

    use only one instance of MusicPlayer

--- BlockBreaker/Assets/Scripts/MusicPlayer.cs
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
+	static MusicPlayer instance = null;
+	
 	// Use this for initialization
 	void Start () {
-		GameObject.DontDestroyOnLoad(gameObject);
+		if(instance != null){
+			Destroy(gameObject);
+			print ("Destroying duplicate instance of Music Player");
+		}else{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
 	}
 	
 	// Update is called once per frame
