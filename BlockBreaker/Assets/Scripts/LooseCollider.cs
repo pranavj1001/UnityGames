d8cb838861d6c38234ec26b1348aa1bc9d82fec5
pranavj1001 <pranavj1001@gmail.com>
2017-08-25 13:11:32 +0530

    use the collider to load a new level

---
+++ BlockBreaker/Assets/Scripts/LooseCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LooseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		print("Triggered");
+		levelManager.LoadLevel("Win");
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision){
+		print("Collision occured");
+	}
+	
+}
