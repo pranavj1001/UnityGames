00bd097a257c4d472f351b7497409d8b464f9b80
pranavj1001 <pranavj1001@gmail.com>
2017-08-27 23:14:42 +0530

    launch the ball after starting the game

--- BlockBreaker/Assets/Scripts/Ball.cs
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -5,6 +5,7 @@ public class Ball : MonoBehaviour {
 
 	public Paddle paddle;
 	private Vector3 paddleToBallVector;
+	private bool hasStarted = false;
 
 	// Use this for initialization
 	void Start () {
@@ -14,13 +15,17 @@ public class Ball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = paddle.transform.position + paddleToBallVector;
 		
-		if(Input.GetMouseButton(0)){
-			print("Mouse Clicked, Launch Ball");
+		if(!hasStarted){
+			//Lock the ball relative to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
 			
-			this.rigidbody2D.velocity = new Vector2(2f, 10f);
-			
+			//Wait for a left-click then launch the ball
+			if(Input.GetMouseButton(0)){
+				print("Mouse Clicked, Launch Ball");
+				hasStarted = true;
+				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+			}
 		}
 	}
 }
