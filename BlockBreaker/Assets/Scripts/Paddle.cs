aaf8561f8eb184b94b9bab91c4b10236e081c7c3
pranavj1001 <pranavj1001@gmail.com>
2017-08-27 22:51:34 +0530

    add script for paddle and move it with the help of mouse

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousePosGameUnits = (Input.mousePosition.x/Screen.width) * 16;
+		Vector3 paddlePos = new Vector3(Mathf.Clamp(mousePosGameUnits, 0.5f, 15.5f), this.transform.position.y, 0f);
+		this.transform.position = paddlePos;
+	}
+}
