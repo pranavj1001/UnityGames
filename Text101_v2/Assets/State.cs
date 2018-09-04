d150cc49fb3185ca82c0b15ebbef777368d85ed0
pranavj1001 <pranavj1001@gmail.com>
2018-09-04 07:59:47 +0530

    add a Scriptable object

---
+++ Text101_v2/Assets/State.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+	[SerializeField] string storyText;
+
+}
